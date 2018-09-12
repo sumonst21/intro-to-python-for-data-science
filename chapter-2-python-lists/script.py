adac48bfff51f6472c62ea9f696ce3fc408b01b6
Md. Sumon Islam <developer.sumon@gmail.com>
2018-09-13 02:38:21 +0600

    Slicing and dicing 2

--- chapter-2-python-lists/script.py
+++ chapter-2-python-lists/script.py
@@ -89,3 +89,14 @@ upstairs = areas[6:]
 print(downstairs)
 print(upstairs)
 print(downstairs + upstairs)
+
+##Slicing and dicing 2
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Alternative slicing to create downstairs
+downstairs = areas[:6]
+print(downstairs)
+# Alternative slicing to create upstairs
+upstairs = areas[6:]
+print(upstairs)
