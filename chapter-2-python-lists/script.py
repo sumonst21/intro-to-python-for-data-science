041aa24fc24752d3520916af3ec7e57450594bb0
Md. Sumon Islam <developer.sumon@gmail.com>
2018-09-13 22:31:10 +0600

    Delete list elements

--- chapter-2-python-lists/script.py
+++ chapter-2-python-lists/script.py
@@ -124,3 +124,11 @@ areas_1 = areas + ["poolhouse", 24.5]
 
 # Add garage data to areas_1, new list is areas_2
 areas_2 = areas_1 + ["garage", 15.45]
+
+
+## Delete list elements
+#### previews areas value with extended list 
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50, "poolhouse", 24.5, "garage", 15.4]
+
+del(areas[-4:-2]) ## this removes poolhouse & its value
