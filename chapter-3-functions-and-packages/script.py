ecc56ac95727ef21cc6e8cb2e5a753e9ac4bf7fb
Md. Sumon Islam <developer.sumon@gmail.com>
2018-09-15 01:31:06 +0600

    Import Package

--- chapter-3-functions-and-packages/script.py
+++ chapter-3-functions-and-packages/script.py
@@ -73,3 +73,21 @@ areas.reverse()
 
 # Print out areas
 print(areas)
+
+
+### Import Package
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * r * math.pi
+
+# Calculate A
+A = math.pi * r ** 2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
